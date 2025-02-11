b0d0a348cd5721e418a7ceead32f8d8361f3da81
Kashish Bhatia <127550842+KashishhBhatia@users.noreply.github.com>
2025-02-11 20:24:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://eventlify.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   }
