c69b0628ee7e58ecb25d892237a0c45610642b32
Kashish Bhatia <127550842+KashishhBhatia@users.noreply.github.com>
2025-02-14 16:47:13 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { AuthProvider } from "./context/AuthProvider.jsx";
 import App from './App.jsx';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://event-lify-backend.onrender.com";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
