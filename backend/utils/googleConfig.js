9ee284d799216eb7595a50ce2c396ceef174d503
Kashish Bhatia <127550842+KashishhBhatia@users.noreply.github.com>
2025-02-16 00:10:38 +0530

    Update googleConfig.js

--- backend/utils/googleConfig.js
+++ backend/utils/googleConfig.js
@@ -15,5 +15,6 @@ export const oauth2Client = new google.auth.OAuth2(
 export const oauth2Clientlogin = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
+    'postmessage',
     
 );
